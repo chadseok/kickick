786fbc05c8621d50742e8ec05002e17aad59f305
Seok-CH <tjr6677@gmail.com>
2023-04-08 13:07:06 +0900

    [#4] feat: 프로젝트 로고 등록
    - 로고를 컴포넌트 처럼 사용하기 위해 src/assets폴더에 위치시켜 사용하고 있습니다
    - 헤더에서 로고svg를 컴포넌트로 가져와 사용하도록 구현하였습니다

--- src/components/features/Layout/Header.tsx
+++ src/components/features/Layout/Header.tsx
@@ -1,5 +1,12 @@
+import { ReactComponent as KickLogo } from 'src/assets/kick_logo.svg';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <KickLogo />
+      <h1 style={{ display: 'none' }}>Kick</h1>
+    </div>
+  );
 };
 
 export default Header;
